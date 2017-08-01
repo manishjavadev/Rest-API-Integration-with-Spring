f94a7890184dcc85f46f33a916537e32d58b84b8
Manish Srivastava <msmanish95@gmail.com>
2017-08-02 01:25:52 +0530

    Spring REST XML and JSON Example manishjavadev

---
+++ spring_with_rest_messenger/src/main/java/com/manish/javadev/controller/MessengerController.java
@@ -0,0 +1,62 @@
+package com.manish.javadev.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manish.javadev.model.UserEntity;
+import com.manish.javadev.service.MessengerService;
+
+/**
+ * @author Manish
+ *
+ */
+@RestController
+@RequestMapping("/messenger")
+public class MessengerController {
+
+	@RequestMapping(value = "/user/{userId}", method = RequestMethod.GET)
+	public ResponseEntity<UserEntity> findUserById(@PathVariable("userId") Integer userId) {
+		UserEntity userEntity = MessengerService.userInfo(userId);
+		return new ResponseEntity<UserEntity>(userEntity, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ResponseEntity<List<UserEntity>> findUsers() {
+		List<UserEntity> userEntity = MessengerService.usersInfo();
+		return new ResponseEntity<List<UserEntity>>(userEntity, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST, produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userEntity) {
+		UserEntity resulltUserEntity = MessengerService.createUser(userEntity);
+		return new ResponseEntity<UserEntity>(resulltUserEntity, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/{userId}", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserEntity> updateUser(@PathVariable("userId") Integer userId,
+			@RequestBody UserEntity userEntity) {
+		UserEntity resulltUserEntity = MessengerService.updateUser(userId, userEntity);
+		return new ResponseEntity<UserEntity>(resulltUserEntity, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/{userId}", method = RequestMethod.DELETE, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, consumes = {
+					MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<UserEntity>> deletUser(@PathVariable("userId") Integer userId) {
+		List<UserEntity> resulltUserEntity = MessengerService.deleteUser(userId);
+		return new ResponseEntity<List<UserEntity>>(resulltUserEntity, HttpStatus.OK);
+	}
+
+}
